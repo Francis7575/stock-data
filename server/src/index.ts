f0e37aec81f8b15d7e14f64561a5b4e401c2eced
Francis Sanchez <francis.albert.2009@gmail.com>
2025-01-24 21:19:47 -0800

    updated index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,20 @@
+import { Request, Response } from "express";
 import app from "./app";
 import { env, logger } from "./config";
 
 const PORT = env.port;
 
+app.get("/server-alive", (req: Request, res: Response) => {
+  try {
+    console.log("hello world");
+    res.status(200).send("Test route working");
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Something went wrong");
+  }
+});
+
 app.listen(PORT, () => {
   logger.info(`Server is running on port: ${PORT}`);
 });
+
